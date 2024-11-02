773436efdb822acfd4c9c12a5a73672206b86a6d
Cannafox <gklodkowski@gmail.com>
2024-11-02 23:41:09 +0100

    init

--- lua/meowboard-nvim/window.lua
+++ lua/meowboard-nvim/window.lua
@@ -1,8 +1,8 @@
 local M = {}
 M.__index = M
 
-M.margin_horizontal = 50
-M.margin_vertical = 0
+M.margin_y = 50
+M.margin_x = 0
 M.border = "rounded"
 M.content = {
   "WOOF!",
@@ -12,12 +12,12 @@ M.content = {
 function M:new(width, height)
   local instance = setmetatable({}, M)
 
-  instance.width = math.floor(width / 2) -- - 2*self.margin_horizontal
-  instance.height = math.floor(height / 2) -- - 2*self.margin_vertical
+  instance.width = math.floor(width / 2) -- - 2*self.margin_y
+  instance.height = math.floor(height / 2) -- - 2*self.margin_x
 
   print(string.format("Width: %d, Height: %d", instance.width, instance.height))
-  instance.position_row = self.margin_horizontal
-  instance.position_col = self.margin_vertical
+  instance.position_row = self.margin_x
+  instance.position_col = self.margin_y
 
   instance.dashboard_buffer = vim.api.nvim_create_buf(false, true)
 
