f5d6c131b3effaed71d1fb525fde63f75f633671
Cannafox <gklodkowski@gmail.com>
2024-11-02 04:23:51 +0100

    test

--- lua/meowboard.lua
+++ lua/meowboard.lua
@@ -1,9 +1,15 @@
 local M = {}
 
 function M.default_config()
-  local defaults = require("defaults")
+  local defaults = require("static.defaults")
 
   return defaults
+end
+
+function M.set_keymaps(keymaps)
+  for _, keymap in ipairs(keymaps) do
+    vim.keymap.set(keymap[1], keymap[2], keymap[3])
+  end
 end
 
 function M.setup(opts)
@@ -12,6 +18,9 @@ function M.setup(opts)
   if opts.verbose then
     M.logger = require("utils.logger").init(debug.getinfo(1,'S').source)
   end
+
+  local default_keymaps = require("static.keymaps")
+  M.set_keymaps(default_keymaps)
 end
 
 return M
---
+++ lua/static/keymaps.lua
@@ -0,0 +1,3 @@
+return {
+  {"n", ":TestBoard", function() print("test") end, desc = "chuj"},
+}
