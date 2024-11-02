5cdec5dffc360b486024d5ccad671c0f70959def
Cannafox <gklodkowski@gmail.com>
2024-11-02 22:33:10 +0100

    test

--- lua/meowboard-nvim/window.lua
+++ lua/meowboard-nvim/window.lua
@@ -1,11 +1,16 @@
 local M = {}
 M.__index = M
 
+M.margin = 2
+
 function M:new(window)
   local instance = setmetatable({}, M)
 
-  instance.width = vim.api.nvim_win_get_width(window) or 10
-  instance.height = vim.api.nvim_win_get_height(window) or 10
+  instance.width = vim.api.nvim_win_get_width(window) - 2*self.margin
+  instance.height = vim.api.nvim_win_get_height(window) - 2*self.margin
+  instance.position_row = self.margin
+  instance.position_col = self.margin
+
   instance.border = "rounded"
 
   return instance
@@ -17,7 +22,7 @@ function M:show()
       relative='win',
       width=self.width,
       height=self.height,
-      col=0, row=0,
+      col=self.position_col, row=self.position_row,
       border = self.border
     })
 end
