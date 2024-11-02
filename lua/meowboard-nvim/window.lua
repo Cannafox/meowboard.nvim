25b5690f9121031eebda85271a98995b63f2523a
Cannafox <gklodkowski@gmail.com>
2024-11-02 22:42:00 +0100

    test

--- lua/meowboard-nvim/window.lua
+++ lua/meowboard-nvim/window.lua
@@ -7,7 +7,7 @@ function M:new(window)
   local instance = setmetatable({}, M)
 
   instance.width = vim.api.nvim_win_get_width(window) - 4*self.margin
-  instance.height = vim.api.nvim_win_get_height(window) - self.margin
+  instance.height = vim.api.nvim_win_get_height(window) - 2*self.margin
   print(string.format("Width: %d, Height: %d", instance.width, instance.height))
   instance.position_row = self.margin
   instance.position_col = self.margin
