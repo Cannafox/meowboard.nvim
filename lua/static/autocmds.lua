feed6d4bf806972b369ad82b86077e4dda8cc117
Cannafox <gklodkowski@gmail.com>
2024-11-02 04:43:48 +0100

    test

--- lua/static/autocmds.lua
+++ lua/static/autocmds.lua
@@ -1,5 +1,5 @@
--- local meowboard = require("meowboard")
---
+local meowboard = require("meowboard")
+
 return {
-  { "MeowboardToggle", function() require("meowboard").toggle() end, { desc = { "test" } } }
+  { "MeowboardToggle", function() meowboard.toggle() end, { desc = { "test" } } }
 }
