6e7433e86cff7b212ff0c9ed4579d83eb3731663
Cannafox <gklodkowski@gmail.com>
2024-11-02 20:17:18 +0100

    test

--- lua/core/window.lua
+++ lua/core/window.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.toggle()
   local buffer = vim.api.nvim_create_buf(false, true)
 
-  vim.api.nvim_open_win(buffer, false, {relative='win', row=5, col=5, width=10, height=2})
+  vim.api.nvim_open_win(buffer, false, {relative='win', row=10, col=10, width=40, height=40})
   -- vim.api.nvim_set_current_buf(buffer)
 end
 
