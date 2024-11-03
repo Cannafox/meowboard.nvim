d6537d2b915b715dd59b519a042bff679eed44cb
Cannafox <gklodkowski@gmail.com>
2024-11-03 01:03:04 +0100

    test

--- lua/meowboard-nvim/window.lua
+++ lua/meowboard-nvim/window.lua
@@ -1,13 +1,7 @@
 local M = {}
 M.__index = M
 
-M.margin_x = 20
-M.margin_y = 4
 M.win_opts = {
-  relative='editor',
-  border = "rounded",
-  style = "minimal",
-  anchor = "NW",
 }
 M.border = "rounded"
 M.content = {
@@ -15,16 +9,23 @@ M.content = {
   "...and MEOW!"
 }
 
+function M:get_default_win_opts()
+  return {
+    relative='editor',
+    border = "rounded",
+    style = "minimal",
+    anchor = "NW",
+  }
+end
+
 function M:new(width, height)
-  print(string.format("Original format: %d x %d", width, height))
   local instance = setmetatable({}, M)
 
-  instance.width = width - 2*self.margin_x
-  instance.height = height - 2*self.margin_y
+  instance.width =  150
+  instance.height = 35
 
-  print(string.format("Margin format: %d x %d", instance.width, instance.height))
-  instance.position_x = self.margin_x
-  instance.position_y = self.margin_y
+  instance.position_x = 20
+  instance.position_y = 4
 
   instance.dashboard_buffer = vim.api.nvim_create_buf(false, true)
 
@@ -37,7 +38,7 @@ function M:show()
   vim.api.nvim_open_win(
     self.dashboard_buffer,
     true,
-    vim.tbl_deep_extend('force', self.win_opts, {
+    vim.tbl_deep_extend('force', self.get_default_win_opts(self), {
       width=self.width,
       height=self.height,
       col=self.position_x,
